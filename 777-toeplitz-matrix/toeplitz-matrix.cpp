2dba5aeae263867cd4467c199a367e5abddcfd12
sivabalanU <118263833+sivabalanU@users.noreply.github.com>
2024-09-20 17:34:14 +0530

    Time: 14 ms (25.72%) | Memory: 20.6 MB (69.67%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.cpp
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -5,48 +5,15 @@ public:
         {
             return true;
         }
-        int prev = matrix[0][0];
-        for(int i=0;i<matrix.size() && i<matrix[0].size();i++)
+        for(int i=1;i<matrix.size();i++)
         {
-            if(prev != matrix[i][i])
+            for(int j=1;j<matrix[i].size();j++)
             {
-                return false;
-            }
-        }
-        int col_count = 1;
-        for(int k=1;k<matrix[0].size() ;k++)
-        {      
-               int i = 0;
-               int j=col_count;
-               prev = matrix[i][j];
-               while(i<matrix.size() && j<matrix[0].size())
-               {
-                    if(prev != matrix[i][j])
-                    {
-                        return false;
-                    }
-                    i++;
-                    j++;
-               }  
-               col_count++;
-        }
-        int row_count = 1;
-        for(int k=1;k<matrix.size();k++)
-        {
-            int i=0;
-            int j=row_count;
-            prev = matrix[j][i];
-            while(i<matrix[0].size() &&j<matrix.size())
-            {
-                if(prev != matrix[j][i])
+                if(matrix[i][j] != matrix[i-1][j-1])
                 {
                     return false;
                 }
-                i++;
-                j++;
             }
-            cout<<endl;
-            row_count++;
         }
         return true;
     }
