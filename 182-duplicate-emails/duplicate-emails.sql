62355bb5213f272700f794a83e10d2a9aa52e81d
sivabalanU <118263833+sivabalanU@users.noreply.github.com>
2024-10-05 16:55:14 +0530

    Time: 343 ms (64.26%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,8 @@
-select  distinct P1.email as Email from Person P1
-inner join Person P2
-on P1.email = P2.email and P1.id <> P2.id
-;
+-- select  distinct P1.email as Email from Person P1
+-- inner join Person P2
+-- on P1.email = P2.email and P1.id <> P2.id
+-- ;
+
+select email as Email from Person
+group by Email
+having(count(*) >1)
